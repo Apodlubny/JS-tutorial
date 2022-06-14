2927b7708948ea5833e1f2a789e333421a3ee504
Apodlubny <apodlubny@gmail.com>
2022-06-14 12:32:49 +0300

    some changes

--- js/index.js
+++ js/index.js
@@ -40,6 +40,23 @@
 // for (let i = 0; i < max; i += 1) {
 //   console.log(`${max} % ${i} = `, max % i);
 // }
-for (let i = 100; i >= 1000; i += 1) {
-  console.log(i);
+
+// for (let i = 5; i <= 50; i += 5) {
+//   console.log(i);
+// }
+
+const max = 10;
+for (let i = 0; i < max; i += 1) {
+  console.log(`${max} % ${i} = `, max % i);
+}
+
+// поиск нечетных чисел
+const number = 125;
+
+for (let i = 0; i < number; i += 1) {
+  if (i % 2 === 0) {
+    continue;
+  }
+
+  console.log("Нечетное i: ", i); // 1, 3, 5, 7, 9
 }
