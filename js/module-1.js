80d64191ccfad74c9be47b4202e0b77f0f4d3499
Apodlubny <apodlubny@gmail.com>
2022-11-15 13:39:27 +0200

    one more simple example

--- js/module-1.js
+++ js/module-1.js
@@ -230,3 +230,8 @@
 //   outputEl.textContent = total;
 // });
 // ============================================================================================
+
+let base = Number(prompt("Давай число"));
+let power = Number(prompt("Давай степень"));
+let res = Math.pow(base, power);
+console.log(res);
